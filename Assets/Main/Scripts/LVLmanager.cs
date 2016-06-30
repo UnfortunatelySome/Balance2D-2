405042236f5cd4d731bd0f83ab74fd959c38d8a8
Jaskaran Bakshi <jas10022jb@gmail.com>
2016-06-29 19:32:42 -0700

    changes

---
+++ Assets/Main/Scripts/LVLmanager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LVLmanager : MonoBehaviour {
+
+	public int level;
+
+	public Text levelText;
+
+	// Use this for initialization
+	void Start () {
+
+		levelText.text = level.ToString();
+	
+	}
+
+
+	public void LevelSelect(){
+
+		Application.LoadLevel ("Level " + level);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
