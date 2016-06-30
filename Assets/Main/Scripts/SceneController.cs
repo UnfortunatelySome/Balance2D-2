82a2d75c06f8edd4bedb4ce4db505f5955cbcd0a
Jaskaran Bakshi <jas10022jb@gmail.com>
2016-06-29 21:16:41 -0700

    changes

---
+++ Assets/Main/Scripts/SceneController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneController : MonoBehaviour {
+
+	public void StartButton (){
+
+		Application.LoadLevel ("Level 0");
+
+	}
+	public void LevelSelectButton(){
+		Application.LoadLevel ("LVL select");
+	}
+	public void SettingsButton(){
+	}
+}
