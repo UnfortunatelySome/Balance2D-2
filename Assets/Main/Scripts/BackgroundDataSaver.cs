2294e1fed1976ecaf857874a53cbabd35e650640
Jaskaran Bakshi <jas10022jb@gmail.com>
2016-07-04 16:55:18 -0700

    changes

---
+++ Assets/Main/Scripts/BackgroundDataSaver.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundDataSaver : MonoBehaviour {
+
+	public static int maxReachedLevel;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (gameObject);
+
+		maxReachedLevel = GameController.maxLevel;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Main/Scripts/LVLmanager.cs
+++ Assets/Main/Scripts/LVLmanager.cs
@@ -5,20 +5,45 @@ using UnityEngine.UI;
 public class LVLmanager : MonoBehaviour {
 
 	public int level;
-
 	public Text levelText;
+	public Sprite Lock;
+	public Sprite Lvl;
+
+	private Image image;
+	private bool LockActive;
 
 	// Use this for initialization
 	void Start () {
 
-		levelText.text = level.ToString();
+		image = gameObject.GetComponent<Image>();
+
+
+		if (BackgroundDataSaver.maxReachedLevel < level) {
+
+			levelText.text = "";
+			image.sprite = Lock;
+			LockActive = true;
+
+
+		} else if (BackgroundDataSaver.maxReachedLevel >= level) {
+
+			levelText.text = level.ToString();
+			image.sprite = Lvl;
+			LockActive = false;
+
+		}
 	
 	}
 
 
 	public void LevelSelect(){
 
-		Application.LoadLevel ("Level " + level);
+		if (LockActive.Equals (false)) {
+
+			Application.LoadLevel ("Level " + level);
+		} else {
+			return;
+		}
 
 	}
 	
